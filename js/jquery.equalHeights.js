6dfdd0c4a27d9e9e5a3f15c9b4959b5a02d4551b
Sean Jones <sean.jones@eqtr.com>
2013-08-09 15:27:08 +0100

    added js

---
+++ js/jquery.equalHeights.js
@@ -0,0 +1,59 @@
+﻿/*!
+ * jquery.equalHeights.js
+
+Description: Basic equalheights plugin. Sets a min-height, or height for legacy browsers, to a collection of items.
+
+Dependancies: jQuery
+
+Example usage:
+
+$(container).equalHeights({target: element});
+
+ */
+
+
+(function ($) {
+
+    /*  Check browser for min-height support.
+        Returns true or undefined
+    */
+    var supportsMinHeight = (function () {
+        if (typeof document.body.style.minHeight !== "undefined")
+            return true;
+    })();
+
+
+    $.fn.equalHeights = function (options) {
+        var settings = $.extend({}, $.fn.equalHeights.defaultOptions, options),
+            greatestHeight = 0,
+            heightCSS = supportsMinHeight ? 'min-height' : 'height';
+
+        return this.each(function () {
+            var container = $(this),
+                $elem = $(settings.target, container); 
+            
+            // For each item within the container
+            $elem.each(function (i) {
+                var $elemHeight = $(this).height();
+
+                // Get the tallest item height
+                if ($elemHeight > greatestHeight) {
+                    greatestHeight = $elemHeight;
+                }
+
+                // Set the height for all items within the container
+                if (i + 1 == $elem.length) {
+                    $elem.css(heightCSS, greatestHeight);
+                }
+            });
+            
+        });
+
+    };
+
+    // Default options
+    $.fn.equalHeights.defaultOptions = {
+        target: '.item'
+    };
+
+})(jQuery);
